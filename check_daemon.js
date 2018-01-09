c547f84307a8cda360955ef518bc4bcc46fb0b57
James D. Wilson <james@jameswilson.name>
2018-01-08 22:07:18 -0600

    Upgraded JS

--- check_daemon.js
+++ check_daemon.js
@@ -1,6 +1,5 @@
 /*jslint node: true */
-"use strict";
-var check_daemon = require('byteballcore/check_daemon.js');
+const check_daemon = require('byteballcore/check_daemon.js');
 
 check_daemon.checkDaemonAndRestart('node merchant.js', 'node merchant.js > log');
 
